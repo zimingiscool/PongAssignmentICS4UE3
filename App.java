a3c53735e805da3b19ef2da578d9ab37c5f509e3
zimingiscool <canadianzim@gmail.com>
2024-11-04 17:02:49 -0500

    Changes n dat

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 import java.awt.Graphics;
 import javax.swing.*;
-public class App {
-
+public class App 
+{
     public static void main(String[] args) 
     {
+        //Window Creation Code
         JFrame gameWindow = new JFrame("Window");
+        gameWindow.setSize(512,512);
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
