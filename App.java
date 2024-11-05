29b4999f84d77d5fd3b5782e47c7b0e8d88294e0
zo <65860568+zimingiscool@users.noreply.github.com>
2024-11-05 13:38:53 -0500

    edits

--- App.java
+++ App.java
@@ -2,18 +2,16 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.geom.Ellipse2D;
 
-public class App 
-{
-    public static void main(String[] args) 
-    {
-        //Window Creation Code
-        JFrame gameWindow = new JFrame("Window");
-        gameWindow.setSize(512,512);
-        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Create Panel 
-        Draw panel = new Draw();
-        gameWindow.add(panel);
+public class App {
+  public static void main(String[] args) {
+    // Window Creation Code
+    JFrame gameWindow = new JFrame("Window");
+    gameWindow.setSize(512, 512);
+    gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Create Panel
+    Draw panel = new Draw();
+    gameWindow.add(panel);
 
-        gameWindow.setVisible(true);
-    }
+    gameWindow.setVisible(true);
+  }
 }
--- Player.java
+++ Player.java
@@ -1,9 +1,8 @@
-public class Player 
-{
-    private String name = "";
-    public Player(String name)
-    {
-        this.name = name;
-    }
-    
+public class Player {
+  private String name = "";
+
+  public Player(String name) {
+    this.name = name;
+  }
+
 }
